740bd06ca7aeb3660812b207197cc4305d623997
Samal <56509582+FransoArbela@users.noreply.github.com>
2024-12-21 20:20:31 +0100

    buil responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Button, Tooltip } from '@chakra-ui/react'
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-
   return (
     <>
-  
+      <Grid templateAreas={{
+        base: '"nav" "main"',
+        lg: '"nav nav" "aside main"',
+      }}>
+        <GridItem area="nav" bg="purple">Nav</GridItem>
+
+        <Show above="lg">
+        <GridItem area="aside" bg="green">Aside</GridItem>
+        </Show>
+        
+        <GridItem area="main" bg="dodgerblue">Nav</GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
